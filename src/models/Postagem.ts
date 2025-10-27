d1db3a429717ee259d952f0b6251924a9e2df5a1
Flavio Serra <sonyflavio@gmail.com>
2025-10-27 11:34:25 -0300

    Criando interfaces model

---
+++ src/models/Postagem.ts
@@ -0,0 +1,10 @@
+import type Tema from "./Tema";
+import type Usuario from "./Usuario";
+
+export default interface Postagem {
+    id: number;
+    titulo: string;
+    data: string;
+    tema: Tema | null;
+    usuario: Usuario | null
+}
