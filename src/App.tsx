35fde54282311b57623b9a55a6b12e91aac10fc1
Flavio Serra <sonyflavio@gmail.com>
2025-10-28 11:48:18 -0300

    Criação da logica - Pagina de login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Cadastro from './pages/cadastro/Cadastro'
-import Login from './pages/login/Login'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
