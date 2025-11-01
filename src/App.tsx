d72481237ba407bfdf948e47dbadbed4c461bac1
Flavio Serra <sonyflavio@gmail.com>
2025-11-01 09:22:24 -0300

    Criando componente CardTema e ListaTemas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/home/Home";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
 import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
