dbcd850e78d23884942991c91fc3ea923fbcb4f6
Flavio Serra <sonyflavio@gmail.com>
2025-11-01 10:11:09 -0300

    Criando o componente FormTema

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
 import { AuthProvider } from "./contexts/AuthContext";
 import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import FormTema from "./components/tema/formtema/FormTema";
 
 function App() {
   return (
@@ -20,6 +21,8 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
             </Routes>
           </div>
           <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -27,7 +27,7 @@ function Navbar() {
                     <div className='flex gap-4'>
                         Postagens
                         <Link to='/temas' className="hover:underline">Temas</Link>
-                        Cadastrar tema
+                        <Link to='/cadastrartema' className="hover:underline">Cadastrar Tema</Link>
                         Perfil
                         <Link to='' onClick={logout} className="hover:underline">Sair</Link>
                     </div>
