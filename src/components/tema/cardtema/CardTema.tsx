0a02723fad8df7f36f3899eded4bcca599548c6f
Flavio Serra <sonyflavio@gmail.com>
2025-11-01 10:03:29 -0300

    Criando a logica CardTema e ListaTemas

--- src/components/tema/cardtema/CardTema.tsx
+++ src/components/tema/cardtema/CardTema.tsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom'
+import type Tema from '../../../models/Tema'
 
-function CardTema() {
+interface CardTemaProps{
+    tema: Tema
+}
+
+function CardTema({tema}: CardTemaProps) {
     return (
-        <div className='border flex flex-col rounded-2x1 overflow-hidden justify-between'>
+        <div className='flex flex-col rounded-2xl overflow-hidden justify-between'>
             <header className='py-2 px-6 bg-indigo-800 text-white font-bold text-2x1'>Tema</header>
-            <p className='p-8 text-3x1 bg-slate-200 h-full'>Descrição</p>
+            <p className='p-8 text-3x1 bg-slate-200 h-full'>{tema.descricao}</p>
 
             <div className="flex">
                 <Link to='' 
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,15 +1,28 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-nest-cnzb.onrender.com'
-})
+  baseURL: "https://blogpessoal-nest-cnzb.onrender.com",
+});
 
-export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data)
-}
+export const cadastrarUsuario = async (
+  url: string,
+  dados: object,
+  setDados: Function
+) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
 
-export const login = async (url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data)
-}
+export const login = async (url: string, dados: object, setDados: Function) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
+
+export const buscar = async (
+  url: string,
+  setDados: Function,
+  header: object
+) => {
+  const resposta = await api.get(url, header);
+  setDados(resposta.data);
+};
