7b0b92dfd98f9d4d255bef4e6ca102fb14dfc326
Flavio Serra <sonyflavio@gmail.com>
2025-11-01 12:36:50 -0300

    Mudanças no footer, redes socias

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,18 +10,12 @@ function Footer() {
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                        Blog Pessoal Generation | Copyright: {data}
+                        Flavio Serra | Copyright: {data}
                     </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <p className='text-lg'>Acesse meu Linkedin</p>
                     <div className='flex gap-2'>
                         <a href="https://www.linkedin.com/in/flavio-serra/" target="_blank">
                             <LinkedinLogoIcon size={48} weight='bold' />
-                        </a>
-                        <a href="https://www.instagram.com/" target="_blank">
-                            <InstagramLogoIcon size={48} weight='bold' />
-                        </a>
-                        <a href="https://www.facebook.com/" target="_blank">
-                            <FacebookLogoIcon size={48} weight='bold' />
                         </a>
                     </div>
                 </div>
